fe1e3f83419f0fa62709caa9aaa218b5d5eb2036
DZMITRY YATSKEVICH <53887281+GoodFellow23@users.noreply.github.com>
2020-02-28 22:27:20 +0300

    basic-js(recursive-depth): add another solution of the task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,8 +2,8 @@ module.exports = class DepthCalculator {
     
     calculateDepth(arr) { 
 
-         if (arr.findIndex(el=>Array.isArray(el))==-1) return 1;         
-         else return 1+this.calculateDepth(arr.flat(1));         
-         
+         return arr.findIndex(el=>Array.isArray(el))==-1? 1: 1+this.calculateDepth(arr.flat(1));           
     }
 };
+
+ 
