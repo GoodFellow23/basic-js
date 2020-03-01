b8bfbb933d575f48fed5f1b24ff0eb45a4e754af
DZMITRY YATSKEVICH <53887281+GoodFellow23@users.noreply.github.com>
2020-03-01 17:29:30 +0300

    basic-js(transform-array): add solution of the task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if((/-prev\b/).test(arr[0])) arr.shift();
+    if((/-next\b/).test(arr[arr.length-1])) arr.pop();
+  arr = arr.map((el,idx,ar) => {
+        if(el=='--double-next') {
+        return ar[idx+1];
+        }
+        else if(el=='--double-prev') {
+            return ar[idx-1];
+        }  
+        else return el;
+      });  
+   while ( arr.indexOf('--discard-next') != -1) {
+       arr.splice(arr.indexOf('--discard-next'),2);
+       if((/-prev\b/).test(arr[0])) arr.shift();
+      
+   }
+   while (arr.indexOf('--discard-prev') != -1) {
+       arr.splice(arr.indexOf('--discard-prev')-1,2);
+       if((/-prev\b/).test(arr[0])) arr.shift();
+     
+   }   
+
+  return arr;
+ 
 };
