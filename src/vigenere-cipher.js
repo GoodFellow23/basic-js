ad1fcd21766bd0b094398ae68aa21506aad438be
DZMITRY YATSKEVICH <53887281+GoodFellow23@users.noreply.github.com>
2020-03-02 18:37:42 +0300

    basic-js(vingenere-cipher): add solution of the task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,44 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        
+     constructor (value=true) {
+        this.direct=value;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    decrypt(str,key) {
+        if(str == undefined || key == undefined) throw new Error();        
+        let temp;
+        let i = 0;
+        str = this.direct ? str.toUpperCase().split('') : str.toUpperCase().split('').reverse();
+        key = this.direct ? key.toUpperCase().split('') : key.toUpperCase().split('').reverse();
+       
+        return str.map((el,idx) => {            
+          if(this.alphabet.indexOf(el) == -1) return el;          
+          else {
+              i = (i < key.length ? i : 0);
+              temp = this.alphabet.length + this.alphabet.indexOf(el) - this.alphabet.indexOf(key[i++]); 
+              return this.alphabet[temp < this.alphabet.length ? temp : temp - this.alphabet.length];             
+            }
+        }).join('');
+    };
+
+    encrypt(str,key) {        
+        if(str==undefined || key==undefined) throw new Error();
+        let temp=0;
+        let i=0;
+        str = this.direct ? str.toUpperCase().split('') : str.toUpperCase().split('').reverse();
+        key = this.direct ? key.toUpperCase().split('') : key.toUpperCase().split('').reverse();
+
+       return str.map((el) => {                      
+          if(!this.alphabet.includes(el)) return el;
+          else {
+              i = (i < key.length ? i : 0);
+              temp = this.alphabet.indexOf(el) + this.alphabet.indexOf(key[i++]);              
+              return this.alphabet[temp < this.alphabet.length ? temp : temp - this.alphabet.length];
+            }            
+        }).join('');
+    };    
 }
-
+   
 module.exports = VigenereCipheringMachine;
