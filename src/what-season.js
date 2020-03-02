53b49747f216f385b7e7315c1b68b399ce4458d6
DZMITRY YATSKEVICH <53887281+GoodFellow23@users.noreply.github.com>
2020-03-03 00:11:27 +0300

    basic-js(what-season): add solution of the task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,14 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+
+    if(!date) return 'Unable to determine the time of year!';
+
+    if(!(Object.prototype.toString.apply(date) === '[object Date]')) throw new Error;
+    if(date.getMonth() >11 || date.getMonth() < 0) throw new Error;    
+    if(2 <= date.getMonth() && date.getMonth() < 5) return 'spring';
+    else if( 5 <= date.getMonth() && date.getMonth() < 8) return 'summer';
+    else if( 8 <= date.getMonth() && date.getMonth() < 11) return 'autumn';
+    else return 'winter';
+    
+   
+    
 };
